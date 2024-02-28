9eec3ab7369f73ace927680e67631eebfc808395
andepraveenofficial <praveenande2023@gmail.com>
2024-02-29 00:33:52 +0530

    Testing deployment

---
+++ src/Layouts/Header.jsx
@@ -0,0 +1,15 @@
+/* -----> Third Party packages <----- */
+import React from 'react'
+
+/* -----> Component <----- */
+const Header = () => {
+    console.log("   Header Layout")
+
+    // Return JSX
+    return (
+        <div>Header</div>
+    )
+}
+
+/* -----> Export <-----*/
+export default Header
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+/* -----> Third Party Packages <-----*/
+import React from 'react'
+
+/* -----> Component <----- */
+const Home = () => {
+    console.log("   Home Page")
+
+    // Return JSX
+    return (
+        <div>Home</div>
+    )
+}
+
+/* -----> Export <----- */
+export default Home

01a843bb621c14dc74dc0ac15ccc0e253af8a902
andepraveenofficial <praveenande2023@gmail.com>
2024-02-29 00:25:50 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* -----> Third party Packages <-----*/
+import React from 'react'
+
+/* -----> Component <----- */
+const App = () => {
+  console.log("App Layout")
+
+
+  // Return JSX
+  return (
+    <div>App</div>
+  )
+}
+
+export default App

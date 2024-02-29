e397a088a78d1355d007caab4a98fe94b60ecffe
andepraveenofficial <praveenande2023@gmail.com>
2024-02-29 20:48:23 +0530

    Update download link for CV

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             <div className="flex flex-col gap-5 md:flex-row">
                 <button className="p-3 text-black bg-white rounded-full" onClick={() => navigate("/contact")}>Contact Me</button>
 
-                <a href="../../public/CV.pdf" target="_blank" download="Ande Praveen Resume" className="p-3 border rounded-full">
+                <a href="./../../public/CV.pdf" target="blank" download="Ande Praveen Resume" className="p-3 border rounded-full">
                     Download CV
                 </a>
 

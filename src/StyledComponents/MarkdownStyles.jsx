2b0721e6bc48afa9f3adb0d8d96ac8d431f19265
andepraveenofficial <praveenande2023@gmail.com>
2024-03-07 15:32:09 +0530

    Update Markdown component styles.

--- src/StyledComponents/MarkdownStyles.jsx
+++ src/StyledComponents/MarkdownStyles.jsx
@@ -1,49 +1,37 @@
 import { styled } from 'styled-components';
 
 const MarkdownStyles = styled.div`
-background-color:#ffffff;
-padding:50px;
-border-radius:8px;
-color:#000000;
-min-height:100%;
-display:flex;
-flex-direction:column;
-align-items:flex-start;
-gap:5px;
-h1{
-    
-    font-size:32px;
-}
-h2{
-    font-size:24px;
-}
-h3{
-    color:gray;
-    font-size:20px;
-}
-h4{
-    font-size:20px;
-}
-h5{
-    color:green;
-    font-size:16px;
-}
-h6{
-    font-size:16px;
-}
-p{
-    font-size:16px;
-}
+    background-color: #ffffff;
+    padding: 50px;
+    border-radius: 8px;
+    color: #000000;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 5px;
 
-ul{
-list-style-type:disc;
-margin-left:25px;
-}
+    h1, h2, h3, h4, h5, h6 {
+        margin-top: 20px;
+        font-weight: bold;
+    }
 
-code{
-    width:100% !important;
-}
+    h1 { font-size: 32px; }
+    h2 { font-size: 24px; }
+    h3 { color: gray; font-size: 20px; }
+    h4 { font-size: 20px; }
+    h5 { color: green; font-size: 16px; }
+    h6 { font-size: 16px; }
 
+    p { margin-top: 5px; font-size: 16px; }
+
+    ul { list-style-type: disc; margin-left: 25px; }
+    ol { list-style-type: number; margin-left: 25px; }
+
+    code { 
+          width:100% !important; 
+       max-width:720px !important;
+    }
 `;
 
 export default MarkdownStyles;

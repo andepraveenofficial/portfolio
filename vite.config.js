3f217bb707e550da085a2f363e35f6c258fff698
andepraveenofficial <praveenande2023@gmail.com>
2024-03-07 22:52:57 +0530

    Modification done on markdown files.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // Markdown 
 import { remarkCodeHike } from "@code-hike/mdx"  // Styles
 
@@ -18,5 +17,6 @@ export default defineConfig(async () => {
         remarkPlugins: [[remarkCodeHike, { theme: "material-palenight" }]],
       }),
     ],
+  
   }
 })

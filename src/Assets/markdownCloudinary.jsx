a209cdebf4a6145f564fdac5ea082dfc4a1548ec
andepraveenofficial <praveenande2023@gmail.com>
2024-03-21 11:25:23 +0530

    CV Update: Email change and experience adjustment - Updated email address in the CV from praveenande2023@gmail.com to praveenande2024@gmail.com - Adjusted work experience dates for the position of Application Engineer at Insightmobi Solutions Pvt Ltd, from Sep 2021 - Present to Dec 2020 - Present
    CSS and HTML markdown images migration to Cloudinary
    - Migrated local image links to Cloudinary for CSS and HTML markdown files
    - Updated image links for background-position, box-properties, positions, Semantic-vs-non-semantic
    
    HTML markdown file changes
    - Updated Semantic-vs-non-semantic image link in HTML markdown file
    
    Deleted local image files
    - Deleted local image files for background-position.jpg, box-properties.jpg, and positions.png as they are now hosted on Cloudinary

---
+++ src/Assets/markdownCloudinary.jsx
@@ -0,0 +1,8 @@
+// HTML 
+export const semantic_vs_nonsemantic = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1710999513/portfolio/markdown/html/semantic-vs-non-semantic.png"
+
+
+// CSS 
+export const background_position = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1710999947/portfolio/markdown/css/background-position.jpg"
+export const box_properties = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1710999949/portfolio/markdown/css/box-properties.jpg"
+export const positions = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1710999953/portfolio/markdown/css/positions.png"

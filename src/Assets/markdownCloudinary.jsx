1120958a08a99d923e22ef003e820318ee0c83ac
andepraveenofficial <praveenande2023@gmail.com>
2024-03-21 16:03:54 +0530

    "Update markdown files with new image URLs for JavaScript concepts"
    This commit updates the image URLs in the markdown files related to JavaScript concepts in the 'src/Assets/markdownFIles/javascript' directory. It replaces the locally stored image files with new URLs from a cloudinary server. The updated images cover a range of topics including hoisting, event loop, microtask queue, debouncing, throttling, capturing, bubbling, async/await, currying, and recursion.

--- src/Assets/markdownCloudinary.jsx
+++ src/Assets/markdownCloudinary.jsx
@@ -6,3 +6,49 @@ export const semantic_vs_nonsemantic = "https://res.cloudinary.com/dwrwbjd3h/ima
 export const background_position = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1710999947/portfolio/markdown/css/background-position.jpg"
 export const box_properties = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1710999949/portfolio/markdown/css/box-properties.jpg"
 export const positions = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1710999953/portfolio/markdown/css/positions.png"
+
+// JAVASCRIPT 
+export const closure = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711006635/portfolio/markdown/javascript/scope/closure.png"
+
+export const code = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711006774/portfolio/markdown/javascript/js_environment/Javascript_Code.jpg"
+export const js_runtime_environment = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711006778/portfolio/markdown/javascript/js_environment/Javascript_Runtime_Environment.jpg"
+export const js_engine = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711006766/portfolio/markdown/javascript/js_environment/Callstack.jpg"
+export const web_apis = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711006781/portfolio/markdown/javascript/js_environment/Web_APIs.jpg"
+export const execution_context = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711006772/portfolio/markdown/javascript/js_environment/Execution__Context.jpg"
+
+
+export const hoisting = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007204/portfolio/markdown/javascript/code_working/hoisting/01_Hoisting.webp"
+export const function_storage = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007206/portfolio/markdown/javascript/code_working/hoisting/02_Function_Storage.gif"
+export const variable_storage = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007210/portfolio/markdown/javascript/code_working/hoisting/03_Variable_Storage.gif"
+export const var_keyword_storage = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007214/portfolio/markdown/javascript/code_working/hoisting/04_var_keyword_Storage.gif"
+export const execution_phase = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007217/portfolio/markdown/javascript/code_working/hoisting/05_Execution_Phase.gif"
+export const undefined_value = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007221/portfolio/markdown/javascript/code_working/hoisting/06_undefined.gif"
+export const reference_error = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007225/portfolio/markdown/javascript/code_working/hoisting/07_Reference_Error.gif"
+export const assign_original_values = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711007239/portfolio/markdown/javascript/code_working/hoisting/08_Assign_Original_Values.gif"
+
+
+export const js_runtime_environment_eventloop = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015404/portfolio/markdown/javascript/code_working/event_loop/01_JS_Runtime_Environment.webp"
+export const callstack = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015405/portfolio/markdown/javascript/code_working/event_loop/02_Callstack.gif"
+export const timer = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015407/portfolio/markdown/javascript/code_working/event_loop/03_Timer.gif"
+export const callback_queue = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015406/portfolio/markdown/javascript/code_working/event_loop/04_Callback_Queue.gif"
+export const event_loop = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015407/portfolio/markdown/javascript/code_working/event_loop/05_Event_Loop.gif"
+export const event_loop_output = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015409/portfolio/markdown/javascript/code_working/event_loop/06_Output.gif"
+export const timer_example = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015411/portfolio/markdown/javascript/code_working/event_loop/07_Timer_Example.gif"
+
+
+export const microtask1 = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015955/portfolio/markdown/javascript/code_working/microtask_queue/microtask1.gif"
+export const microtask2 = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015956/portfolio/markdown/javascript/code_working/microtask_queue/microtask2.gif"
+export const microtask3 = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015957/portfolio/markdown/javascript/code_working/microtask_queue/microtask3.gif"
+export const microtask5 = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015962/portfolio/markdown/javascript/code_working/microtask_queue/microtask4.gif"
+export const microtask4 = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015962/portfolio/markdown/javascript/code_working/microtask_queue/microtask5.gif"
+export const microtask7 = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015979/portfolio/markdown/javascript/code_working/microtask_queue/microtask6.gif"
+export const microtask6 = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711015967/portfolio/markdown/javascript/code_working/microtask_queue/microtask7.gif"
+
+
+export const debouncing = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711016632/portfolio/markdown/javascript/core_javascript/debouncing.gif"
+export const throttle = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711016644/portfolio/markdown/javascript/core_javascript/throttle.gif"
+export const throttle_vs_debouncing = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711016645/portfolio/markdown/javascript/core_javascript/throttling-vs-debounce.png"
+export const capturing = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711016601/portfolio/markdown/javascript/core_javascript/Capturing.png"
+export const bubbling = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711017014/portfolio/markdown/javascript/core_javascript/bubbling.png"
+export const async_vs_defer = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711016626/portfolio/markdown/javascript/core_javascript/async_vs_defer.avif"
+export const recursively = "https://res.cloudinary.com/dwrwbjd3h/image/upload/v1711016636/portfolio/markdown/javascript/core_javascript/recursively.png"
